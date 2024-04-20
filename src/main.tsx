2b4fdef938513a66c9315d6517fb251aa8f12593
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-19 20:57:34 -0400

    Build Color Mode Switch

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import theme from "./theme";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode="={theme.config.initialColorMode}" />
       <App />
     </ChakraProvider>
   </React.StrictMode>
