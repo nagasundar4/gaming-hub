b8d8bbce52361cd46536823221e84c4eefaa177b
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-22 18:59:24 -0400

    Add ExpandableText component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,31 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {
+  children: string;
+}
+const ExpandableText = ({ children }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+  const limit = 300;
+  if (!children) return null;
+
+  if (children.length <= limit) return <Text>{children}</Text>;
+
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+  return (
+    <Text>
+      {summary}
+      <Button
+        size="xs"
+        fontWeight="bold"
+        marginLeft={1}
+        colorScheme="yellow"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Load More"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
